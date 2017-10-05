8f7220a5af9ce61f6bfd6c963fb40238d8817903
elhmn <elhmn@localhost.localdomain>
2017-10-05 12:37:15 +0200

    allegro lib interface v1 implemented

--- allegro/inc/DlAllegro.class.hpp
+++ allegro/inc/DlAllegro.class.hpp
@@ -14,10 +14,95 @@
 # define DLALLEGRO_CLASS_HPP
 
 #include "IDl.class.hpp"
+#include "allegro5/allegro.h"
+#include "allegro5/allegro_primitives.h"
+
+typedef struct	s_color
+{
+	int			r;
+	int			g;
+	int			b;
+	int			a;
+}				t_color;
+
+typedef	struct	s_rect
+{
+	int			x;
+	int			y;
+	int			w;
+	int			h;
+}				t_rect;
+
+//black
+t_color					g_col_black = {
+					0,
+					0,
+					0,
+					255
+};
+extern t_color			g_col_black;
+
+//white
+t_color					g_col_white = {
+					255,
+					255,
+					255,
+					255
+};
+extern t_color			g_col_white;
+
+//red
+t_color					g_col_pa = {
+					255,
+					0,
+					0,
+					255
+};
+extern t_color			g_col_pa;
+
+//green
+t_color					g_col_pb = {
+					0,
+					255,
+					0,
+					255
+};
+extern t_color			g_col_pb;
+
+//blue
+t_color					g_col_pc = {
+					0,
+					0,
+					255,
+					255
+};
+extern t_color			g_col_pc;
+
+//yellow
+t_color					g_col_pd = {
+					255,
+					255,
+					0,
+					255
+};
+extern t_color			g_col_pd;
+
+t_color					g_col_bonus = {
+					255,
+					0,
+					255,
+					255
+};
+extern t_color			g_col_bonus;
 
 class		DlAllegro : public IDl
 {
 	public:
+	ALLEGRO_DISPLAY			*win;
+	ALLEGRO_EVENT_QUEUE		*e_queue;
+	ALLEGRO_EVENT			*event;
+	ALLEGRO_BITMAP			*area;
+
 //constructors
 	DlAllegro(void);
 	DlAllegro(DlAllegro const &rhs);
